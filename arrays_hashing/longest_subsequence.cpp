fae827f1ff6c588d2b85c651fdf3306419d38b99
squirrelcoding <rosilescarlos28@gmail.com>
2025-07-09 22:51:05 -0500

    efrgt

--- arrays_hashing/longest_subsequence.cpp
+++ arrays_hashing/longest_subsequence.cpp
@@ -7,34 +7,13 @@ using namespace std;
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-      if (nums.size() == 0) return 0;
 
-      map<int, int> frequency_table;
+      set<int> frequency_table;
       for (int i = 0; i < nums.size(); ++i) {
-        if (frequency_table.count(nums[i]) > 0) {
-          frequency_table[nums[i]]++;
-        } else {
-          frequency_table.insert({nums[i], 1});
-        }
-      }
+        frequency_table.insert(nums[i]);
+      }       
 
-      vector<int> keys;
-      for (auto p : frequency_table) {
-        keys.push_back(p.first);
-      }
-
-      int longest_subsequence = 1;
-      int temp = 1;
-
-      // Apply a sliding window approach
-      for (int i = 0; i < keys.size() - 1; ++i) {
-        while (i < keys.size() - 1 && keys[i] + 1 == keys[i + 1]) {
-          temp += 1; 
-          i++;
-        } 
-        longest_subsequence = max(longest_subsequence, temp);
-        temp = 1;
-      }
+      int longest_subsequence = nums.size();
 
       return longest_subsequence;
     }
